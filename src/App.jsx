e978f45184affa1a449726458ac44af61767c477
hsz <santinogz131@gmail.com>
2024-11-08 16:54:53 -0300

    RUN

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import routes from './routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Login from './components/Login';
+import AdminView from './components/AdminView';
+import ProfessorView from './components/ProfessorView';
+import StudentView from './components/StudentView';
 
 const App = () => {
     return (
-        <Router>
-            {routes()}
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/admin" element={<AdminView />} />
+                    <Route path="/professor" element={<ProfessorView />} />
+                    <Route path="/student" element={<StudentView />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 };
 
 export default App;
-
